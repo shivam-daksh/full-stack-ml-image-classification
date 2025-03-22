db25cef3e5d01731bc7d456bfdce1c676575ce54
Shivam Daksh <shivamdakshshivam@gmail.com>
2025-03-22 11:57:08 +0530

    deploy using gh-pages and vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://shivam-daksh.github.io/full-stack-ml-image-classification/",
   optimizeDeps: {
     exclude: ["lucide-react"],
-  },
-  build: {
-    outDir: ".",
   },
 });
